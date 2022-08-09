37dd4d0f2ce98e7f319dcbffa0107cd6ff3da25d
MaddyBoudreau <gooseboudreau@gmail.com>
2022-08-09 17:19:43 -0400

    first commit

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,15 +1,10 @@
-import imp
-from flask import Flask, render_template
+from flask import render_template
 from . import bp as app
-    
-#REGISTER RENDERING
-
-@app.route("/register")
-def about():
-    return render_template('register.html')
-
-#LOGIN RENDERING
 
 @app.route("/login")
-def contact():
+def login():
     return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 from app import create_app
 
-#this is where we are calling the init page 
-
 app = create_app()
 
 
